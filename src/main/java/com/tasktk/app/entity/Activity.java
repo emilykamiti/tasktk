1f23441be6e54a53d007eec26c2226d7c061df72
emilykamiti <emilykamiti@gmail.com>
2025-06-12 23:51:47 +0300

    added messageEntity

--- src/main/java/com/tasktk/app/entity/Activity.java
+++ src/main/java/com/tasktk/app/entity/Activity.java
@@ -1,4 +1,43 @@
 package com.tasktk.app.entity;
 
-public class Activity {
+
+import java.util.Date;
+
+public class Activity extends BaseEntity {
+    //user_id
+    //task_id
+
+    private Type type;
+    private Date timeStamp;
+    private String details;
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public enum Type{
+        created,
+        updated,
+        completed
+    }
 }
--- src/main/java/com/tasktk/app/entity/Message.java
+++ src/main/java/com/tasktk/app/entity/Message.java
@@ -1,4 +1,35 @@
 package com.tasktk.app.entity;
 
-public class Message {
+import java.util.Date;
+
+public class Message extends BaseEntity{
+    //team_id
+    //sender_id
+    private String content;
+    private Date timeStamp;
+    private Boolean is_read;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public Boolean getIs_read() {
+        return is_read;
+    }
+
+    public void setIs_read(Boolean is_read) {
+        this.is_read = is_read;
+    }
 }
